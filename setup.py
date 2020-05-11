d520ffda073c0b1e7f98225d4af6134b3e223398
BadPixxel <eshop.bpaquier@gmail.com>
2020-05-11 14:01:56 +0200

    ADD: Tags Deploy

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='splashpy',
-       version="0.1.1",
+       version="0.1.3",
        packages=find_packages(),
        install_requires=["pycryptodome", 'crypto', 'pysimplesoap'],
        author='SplashSync',
