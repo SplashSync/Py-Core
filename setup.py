a67a75da6e64b2dacf78cf84a49364566cb90f13
BadPixxel <eshop.bpaquier@gmail.com>
2020-05-26 20:23:40 +0200

    FIX: Remove Dependency on PySimpleSoap

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='splashpy',
-       version="0.1.4",
+       version="0.2.0",
        packages=find_packages(),
        install_requires=["pycryptodome", 'crypto'],
        author='SplashSync',
