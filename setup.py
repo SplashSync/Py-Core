9baa0d693307ab27d7323a98a3d4bc1c36f8124d
BadPixxel <eshop.bpaquier@gmail.com>
2020-05-26 15:00:08 +0200

    FIX: Reading res.company on client init

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='splashpy',
-       version="0.1.3",
+       version="0.1.4",
        packages=find_packages(),
        install_requires=["pycryptodome", 'crypto', 'pysimplesoap'],
        author='SplashSync',
