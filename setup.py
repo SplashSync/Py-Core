c8d6f1b225153704c90d469fa25f62ae117c2051
BadPixxel <eshop.bpaquier@gmail.com>
2020-06-16 12:05:59 +0200

    ADD: Odoo Assets for Splash

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
        name='splashpy',
-       version="0.2.1",
+       version="0.2.2",
        packages=find_packages(),
        install_requires=["pycryptodome", 'crypto'],
        author='SplashSync',
